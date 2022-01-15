b7d18bfa0956089234796d6d12980209ab002223
Balachandran <bala2cisco@gmail.com>
2022-01-15 17:33:27 +0530

    Initial commit

---
+++ src/main/java/FizzBuzz.java
@@ -0,0 +1,6 @@
+public class FizzBuzz {
+    public static int getResult(int number) {
+
+        return number;
+    }
+}
---
+++ src/test/java/FizzBuzzTest.java
@@ -0,0 +1,16 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.Assert;
+import org.junit.jupiter.api.Test;
+
+public class FizzBuzzTest {
+
+    @Test
+    public void printNumber(){
+        Assert.assertEquals(1,FizzBuzz.getResult(1));
+    }
+
+
+
+
+}
