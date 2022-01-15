c1e9411f20d255a56b4bb0d5114951c32ff3984a
Balachandran <bala2cisco@gmail.com>
2022-01-15 17:44:50 +0530

    Refactored Test method name

--- src/test/java/FizzBuzzTest.java
+++ src/test/java/FizzBuzzTest.java
@@ -6,7 +6,7 @@ import org.junit.jupiter.api.Test;
 public class FizzBuzzTest {
 
     @Test
-    public void printNumber(){
+    public void testNumberOne(){
         Assert.assertEquals(1,FizzBuzz.getResult(1));
     }
 
