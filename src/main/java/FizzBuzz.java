a999a8aa877851113f391cff9389e57fb05d3b99
Yuvaraj Muthuraman <yuvamcse@gmail.com>
2022-01-15 14:39:16 +0100

    extracted constant and formatted lines

--- src/main/java/FizzBuzz.java
+++ src/main/java/FizzBuzz.java
@@ -1,9 +1,13 @@
 public class FizzBuzz {
+
+    public static final String FIZZ = "Fizz";
+    public static final String BUZZ = "Buzz";
+
     public static String getResult(int number) {
         if (number % 3 == 0)
-            return "Fizz";
+            return FIZZ;
         else if (number == 5)
-            return "Buzz";
+            return BUZZ;
 
         return String.valueOf(number);
     }
--- src/test/java/FizzBuzzTest.java
+++ src/test/java/FizzBuzzTest.java
@@ -1,30 +1,34 @@
 import org.junit.jupiter.api.Test;
+
 import static org.junit.Assert.assertEquals;
 
 public class FizzBuzzTest {
 
+    public static final String BUZZ = "Buzz";
+    public static final String FIZZ = "Fizz";
+
     @Test
-    public void testNumberOne(){
-        assertEquals("1",FizzBuzz.getResult(1));
+    public void testNumberOne() {
+        assertEquals("1", FizzBuzz.getResult(1));
     }
 
     @Test
-    public void testNumberTwo(){
-        assertEquals("2",FizzBuzz.getResult(2));
+    public void testNumberTwo() {
+        assertEquals("2", FizzBuzz.getResult(2));
     }
 
     @Test
-    public void testNumberThree(){
-        assertEquals("Fizz",FizzBuzz.getResult(3));
+    public void testNumberThree() {
+        assertEquals(FIZZ, FizzBuzz.getResult(3));
     }
 
     @Test
     public void testNumberFive() {
-        assertEquals("Buzz", FizzBuzz.getResult(5));
+        assertEquals(BUZZ, FizzBuzz.getResult(5));
     }
 
     @Test
     public void testNumberSix() {
-        assertEquals("Fizz", FizzBuzz.getResult(6));
+        assertEquals(FIZZ, FizzBuzz.getResult(6));
     }
 }
