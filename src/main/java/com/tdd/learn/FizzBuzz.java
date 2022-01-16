e017a8634641a3854494e788fb2dc161c1d86368
Yuvaraj Muthuraman <yuvamcse@gmail.com>
2022-01-16 18:00:54 +0100

    fixed the invalid value of constant

--- src/main/java/com/tdd/learn/FizzBuzz.java
+++ src/main/java/com/tdd/learn/FizzBuzz.java
@@ -6,7 +6,7 @@ class FizzBuzz {
 
     public static final String FIZZ = "Fizz";
     public static final String BUZZ = "Buzz";
-    public static final String FIZZ_BUZZ = "com.tdd.learn.FizzBuzz";
+    public static final String FIZZ_BUZZ = "FizzBuzz";
 
     public static String getResult(int number) {
         if (isFizzBuzz(number))
--- src/test/java/com/tdd/learn/FizzBuzzTest.java
+++ src/test/java/com/tdd/learn/FizzBuzzTest.java
@@ -9,7 +9,7 @@ class FizzBuzzTest {
 
     public static final String BUZZ = "Buzz";
     public static final String FIZZ = "Fizz";
-    public static final String FIZZ_BUZZ = "com.tdd.learn.FizzBuzz";
+    public static final String FIZZ_BUZZ = "FizzBuzz";
 
     @Test
     void testNumberOne() {
