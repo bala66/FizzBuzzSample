1a13e2813817b993217de0e16fdfb14de17c9b2f
Balachandran <bala2cisco@gmail.com>
2022-01-16 12:01:07 +0530

    Use case & Test Case implementation of sequential numbers. Added Constants

--- src/main/java/FizzBuzz.java
+++ src/main/java/FizzBuzz.java
@@ -1,5 +1,7 @@
 public class FizzBuzz {
 
+    private static final int MIN_VALUE = 1;
+    private static final int MAX_VALUE = 100;
     public static final String FIZZ = "Fizz";
     public static final String BUZZ = "Buzz";
 
@@ -12,5 +14,11 @@ public class FizzBuzz {
             result+= BUZZ;
 
         return result.isEmpty() ? "" + String.valueOf(number) : result;
+    }
+
+    public static void buildOutput() {
+        for (int index = MIN_VALUE; index <= MAX_VALUE; index++) {
+            System.out.println(getResult(index));
+        }
     }
 }
