71541625ebc3f951b98ebf6c862c35213bb9db0b
Balachandran <bala2cisco@gmail.com>
2022-01-16 12:00:50 +0530

    Use case & Test Case implementation of sequential numbers. Added Constants

--- src/test/java/FizzBuzzTest.java
+++ src/test/java/FizzBuzzTest.java
@@ -1,11 +1,13 @@
 import org.junit.jupiter.api.Test;
-
 import static org.junit.Assert.assertEquals;
 
 public class FizzBuzzTest {
 
+    private static final int MIN_VALUE = 1;
+    private static final int MAX_VALUE = 100;
     public static final String BUZZ = "Buzz";
     public static final String FIZZ = "Fizz";
+
 
     @Test
     public void testNumberOne() {
@@ -42,4 +44,21 @@ public class FizzBuzzTest {
         assertEquals(FIZZ+BUZZ, FizzBuzz.getResult(15));
     }
 
+    @Test
+    public void testNumberThirty() {
+        assertEquals(FIZZ+BUZZ, FizzBuzz.getResult(30));
+    }
+
+    @Test
+    public void testSequenceOneToHundred(){
+        for(int index=MIN_VALUE ; index<=MAX_VALUE; index++){
+            if(index % 3 == 0 && index % 5 == 0)
+                assertEquals(FIZZ+BUZZ, FizzBuzz.getResult(index));
+            else if(index % 3 == 0)
+                assertEquals(FIZZ, FizzBuzz.getResult(index));
+            else if(index % 5 == 0)
+                assertEquals(BUZZ, FizzBuzz.getResult(index));
+
+        }
+    }
 }
